64f932eeb75359b099d1debd1dfc1fcfad08f1ef
Kevin Alford <zeroeth@pixelflow.org>
2013-11-04 22:59:51 -0600

    Working objective c version!

--- objectivity/objectivity.m
+++ objectivity/objectivity.m
@@ -1,9 +1,11 @@
 // pure objective c version, and then gnustep version
 #include <Foundation/Foundation.h>
 
-@interface Player : NSObject {
+@interface Player : NSObject
+{
   NSString *name;
 }
+
 
 @property (retain) NSString *name;
 
@@ -14,8 +16,11 @@
 @implementation Player
 @synthesize name;
 
+- (id) initWithName: (NSString *)aName
+{
+  self = [super init];
 
-- (id) initWithName: (NSString *)inName {
+  [self setName:aName];
 
   return self;
 }
@@ -23,21 +28,39 @@
 
 
 
-@interface GameState : NSObject {
-  NSArray *players;
+@interface GameState : NSObject
+{
+  NSMutableArray *players;
 }
 
-@property (retain) NSArray *players;
+@property (retain) NSMutableArray *players;
+
+- (void) addPlayer: (Player *)aPlayer;
 @end
 
 
 @implementation GameState
 @synthesize players;
+
+- (id) init
+{
+  self = [super init];
+
+  [self setPlayers: [[NSMutableArray alloc] init]];
+  return self;
+}
+
+
+- (void) addPlayer: (Player *)aPlayer
+{
+  [self.players addObject:aPlayer];
+}
 @end
 
 
 
-@interface PlayerReport : NSObject {
+@interface PlayerReport : NSObject
+{
 }
 
 + (void) about: (GameState *)level;
@@ -46,8 +69,15 @@
 
 @implementation PlayerReport
 
-+ (void) about: (GameState *)level {
-  NSLog (@"Players");
++ (void) about: (GameState *)level
+{
+  NSLog(@"%u Players", (unsigned int)[[level players] count]);
+  NSLog (@"----------");
+
+  for (Player *player in [level players])
+  {
+    NSLog (@"%@", [player name]);
+  }
 }
 @end
 
@@ -55,13 +85,17 @@
 
 int main( int argc, const char *argv[] )
 {
+  NSAutoreleasePool *pool = [NSAutoreleasePool new];
+
   GameState *level = [[GameState alloc] init];
 
-  Player *player1 = [[Player alloc] initWithName: @"zeroeth"];
-  Player *player2 = [[Player alloc] initWithName: @"takeshi"];
-  Player *player3 = [[Player alloc] initWithName: @"haiiro" ];
+  [level addPlayer: [[Player alloc] initWithName: @"zeroeth"]];
+  [level addPlayer: [[Player alloc] initWithName: @"takeshi"]];
+  [level addPlayer: [[Player alloc] initWithName: @"haiiro" ]];
 
   [PlayerReport about:level];
+
+  [pool drain];
 
   return 0;
 }
