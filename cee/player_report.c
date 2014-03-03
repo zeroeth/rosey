9aa2af72e9d676e6191f6342af956de3b3d439a7
Kevin Alford <zeroeth@pixelflow.org>
2014-03-02 18:20:35 -0600

    Fix gcc warning

--- cee/player_report.c
+++ cee/player_report.c
@@ -5,7 +5,8 @@
 
 void player_report(GameState *game_state)
 {
-	for(int i = 0; i < game_state->player_position; i++)
+	int i;
+	for(i = 0; i < game_state->player_position; i++)
 	{
 		printf("Player: %s\n", game_state->players[i]->name);
 	}
